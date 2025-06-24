45ac075ee04776059693a47fb82670ec71315d21
thiago-oliva <tseoliva@gmail.com>
2025-06-23 22:48:15 -0300

    arreglo rapido

--- tu-proyecto-react/src/App.jsx
+++ tu-proyecto-react/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Products from './components/Products';
+import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
