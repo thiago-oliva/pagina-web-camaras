80a3f535d494335c9091512b3be3c7ca608f6963
thiago-oliva <tseoliva@gmail.com>
2025-08-20 03:26:33 -0300

    perfecto, falta db

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ import EmailConfirmed from './components/EmailConfirmed';
 import LogoutAnimation from './components/LogoutAnimation';
 import AuthRedirect from './components/AuthRedirect';
 import LoadingScreen from './components/LoadingScreen';
-import FloatingCartButton from './components/FloatingCartButton';
 import './components/CartAnimation.css';
 
 function AppContent() {
@@ -154,7 +153,6 @@ function AppContent() {
       </main>
       <Footer />
       <WhatsAppButton />
-      <FloatingCartButton />
     </>
   );
 }
